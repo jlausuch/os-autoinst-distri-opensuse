917150916068211269b107fa6a3e4a9deca9d48d
jlausuch <jalausuch@suse.com>
2023-05-12 23:41:01 +0200

    OpenShift simple container image deployment

--- tests/containers/openshift_image.pm
+++ tests/containers/openshift_image.pm
@@ -13,7 +13,23 @@ use serial_terminal 'select_serial_terminal';
 
 sub run {
     select_serial_terminal;
-    record_info("TBD");
+    my $image = get_required_var('CONTAINER_IMAGE_TO_TEST');
+    my $cmd = '"cat", "/etc/os-release"';
+    my $job_name = "test";
+
+    select_console "user-console";
+
+    # Login to the deployment
+    assert_script_run('eval $(crc oc-env)');
+    assert_script_run('oc login -u developer https://api.crc.testing:6443');
+
+    # Create kubernetes cluster from manifest
+    assert_script_run("curl -O " . data_url("containers/k8s_job_manifest.yaml"));
+    file_content_replace("k8s_job_manifest.yaml", JOB_NAME => $job_name, IMAGE => $image, CMD => $cmd);
+    assert_script_run("oc create -f k8s_job_manifest.yaml", timeout => 600);
+
+    # Various OpenShift operations with the image
+    assert_script_run("oc import-image $image");
 }
 
 1;
