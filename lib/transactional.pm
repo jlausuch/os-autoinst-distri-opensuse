5e8aa68ed23de64d3ad283c0475565cd54bf4e76
Jose Lausuch <jalausuch@suse.com>
2023-09-20 14:13:52 +0200

    Fix Leap Micro UTT repo

--- lib/transactional.pm
+++ lib/transactional.pm
@@ -41,12 +41,7 @@ our @EXPORT = qw(
 
 # Download files needed for transactional update tests
 sub get_utt_packages {
-    # SLE and SUSE MicroOS need an additional repo for testing
-    if (is_sle || is_sle_micro || is_alp) {
-        assert_script_run 'curl -O ' . data_url("microos/utt.repo");
-    } elsif (is_leap_micro) {
-        assert_script_run 'curl -o utt.repo ' . data_url("microos/utt-leap.repo");
-    }
+    assert_script_run 'curl -O ' . data_url("microos/utt.repo");
 
     # Different testfiles for SUSE MicroOS and openSUSE MicroOS
     my $tarball = 'utt-';
